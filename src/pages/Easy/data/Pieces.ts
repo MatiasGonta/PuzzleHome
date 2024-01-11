960d13de9a660b3d36c6171c4929722c1f67bb1d
Matías Gonta <maret2023@gmail.com>
2024-01-11 18:25:06 -0300

    The local storage of the images was changed to the cloud using Cloudinary :bricks:

--- src/pages/Easy/data/Pieces.ts
+++ src/pages/Easy/data/Pieces.ts
@@ -3,145 +3,145 @@ import { Image } from '@/models';
 export const easyPieces:Image[] = [
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece1.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005723/PuzzleHome/Easy/piece1_aqcndh.jpg`,
       alt: "piece1.jpg",
       id: "1"
     },  
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece2.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005727/PuzzleHome/Easy/piece2_ewpyms.jpg`,
       alt: "piece2.jpg",
       id: "2"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece3.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005729/PuzzleHome/Easy/piece3_njn0tm.jpg`,
       alt: "piece3.jpg",
       id: "3"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece4.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005732/PuzzleHome/Easy/piece4_etsxu2.jpg`,
       alt: "piece4.jpg",
       id: "4"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece5.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005735/PuzzleHome/Easy/piece5_cawimh.jpg`,
       alt: "piece5.jpg",
       id: "5"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece6.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005738/PuzzleHome/Easy/piece6_x1b0sk.jpg`,
       alt: "piece6.jpg",
       id: "6"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece7.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005742/PuzzleHome/Easy/piece7_x7bfvr.jpg`,
       alt: "piece7.jpg",
       id: "7"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece8.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005750/PuzzleHome/Easy/piece8_zaqocy.jpg`,
       alt: "piece8.jpg",
       id: "8"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece9.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005753/PuzzleHome/Easy/piece9_c55qol.jpg`,
       alt: "piece9.jpg",
       id: "9"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece10.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005778/PuzzleHome/Easy/piece10_uz4tqs.jpg`,
       alt: "piece10.jpg",
       id: "10"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece11.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005781/PuzzleHome/Easy/piece11_lrhekk.jpg`,
       alt: "piece11.jpg",
       id: "11"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece12.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005784/PuzzleHome/Easy/piece12_pcoxu2.jpg`,
       alt: "piece12.jpg",
       id: "12"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece13.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005604/PuzzleHome/Easy/piece13_vaufuc.jpg`,
       alt: "piece13.jpg",
       id: "13"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece14.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005606/PuzzleHome/Easy/piece14_zivhvu.jpg`,
       alt: "piece14.jpg",
       id: "14"
     },
         {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece15.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005609/PuzzleHome/Easy/piece15_safjnl.jpg`,
       alt: "piece15.jpg",
       id: "15"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece16.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005611/PuzzleHome/Easy/piece16_djzjjx.jpg`,
       alt: "piece16.jpg",
       id: "16"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece17.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005614/PuzzleHome/Easy/piece17_ihwsqf.jpg`,
       alt: "piece17.jpg",
       id: "17"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece18.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005620/PuzzleHome/Easy/piece18_aj9ldb.jpg`,
       alt: "piece18.jpg",
       id: "18"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece19.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005631/PuzzleHome/Easy/piece19_c17srk.jpg`,
       alt: "piece19.jpg",
       id: "19"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece20.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005633/PuzzleHome/Easy/piece20_nejwrd.jpg`,
       alt: "piece20.jpg",
       id: "20"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece21.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005635/PuzzleHome/Easy/piece21_whzcxt.jpg`,
       alt: "piece21.jpg",
       id: "21"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece22.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005638/PuzzleHome/Easy/piece22_gfpqml.jpg`,
       alt: "piece22.jpg",
       id: "22"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece23.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005641/PuzzleHome/Easy/piece23_af93qm.jpg`,
       alt: "piece23.jpg",
       id: "23"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Easy/assets/piece24.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705005643/PuzzleHome/Easy/piece24_j97lcj.jpg`,
       alt: "piece24.jpg",
       id: "24"
     }
--- src/pages/Hard/data/Pieces.ts
+++ src/pages/Hard/data/Pieces.ts
@@ -3,145 +3,145 @@ import { Image } from '@/models';
 export const hardPieces:Image[] = [
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece1.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006623/PuzzleHome/Hard/piece1_ag24wl.jpg`,
       alt: "piece1.jpg",
       id: "1"
     },  
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece2.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006626/PuzzleHome/Hard/piece2_dgytky.jpg`,
       alt: "piece2.jpg",
       id: "2"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece3.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006629/PuzzleHome/Hard/piece3_q6lnwc.jpg`,
       alt: "piece3.jpg",
       id: "3"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece4.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006632/PuzzleHome/Hard/piece4_krcwa1.jpg`,
       alt: "piece4.jpg",
       id: "4"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece5.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006635/PuzzleHome/Hard/piece5_bpbcq1.jpg`,
       alt: "piece5.jpg",
       id: "5"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece6.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006638/PuzzleHome/Hard/piece6_bnjfno.jpg`,
       alt: "piece6.jpg",
       id: "6"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece7.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006641/PuzzleHome/Hard/piece7_lozeat.jpg`,
       alt: "piece7.jpg",
       id: "7"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece8.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006644/PuzzleHome/Hard/piece8_rkcyy3.jpg`,
       alt: "piece8.jpg",
       id: "8"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece9.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006647/PuzzleHome/Hard/piece9_geyddb.jpg`,
       alt: "piece9.jpg",
       id: "9"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece10.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006650/PuzzleHome/Hard/piece10_eys6dl.jpg`,
       alt: "piece10.jpg",
       id: "10"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece11.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006653/PuzzleHome/Hard/piece11_hlkq1p.jpg`,
       alt: "piece11.jpg",
       id: "11"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece12.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006656/PuzzleHome/Hard/piece12_lo9wmc.jpg`,
       alt: "piece12.jpg",
       id: "12"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece13.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006659/PuzzleHome/Hard/piece13_hjntxu.jpg`,
       alt: "piece13.jpg",
       id: "13"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece14.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006662/PuzzleHome/Hard/piece14_ljkgmk.jpg`,
       alt: "piece14.jpg",
       id: "14"
     },
         {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece15.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006665/PuzzleHome/Hard/piece15_sb8eno.jpg`,
       alt: "piece15.jpg",
       id: "15"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece16.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006668/PuzzleHome/Hard/piece16_vmnson.jpg`,
       alt: "piece16.jpg",
       id: "16"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece17.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006672/PuzzleHome/Hard/piece17_cajlg4.jpg`,
       alt: "piece17.jpg",
       id: "17"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece18.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006675/PuzzleHome/Hard/piece18_smelib.jpg`,
       alt: "piece18.jpg",
       id: "18"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece19.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006678/PuzzleHome/Hard/piece19_ovamga.jpg`,
       alt: "piece19.jpg",
       id: "19"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece20.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006506/PuzzleHome/Hard/piece20_vcotgv.jpg`,
       alt: "piece20.jpg",
       id: "20"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece21.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006520/PuzzleHome/Hard/piece21_y8acvd.jpg`,
       alt: "piece21.jpg",
       id: "21"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece22.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006615/PuzzleHome/Hard/piece22_gttw6m.jpg`,
       alt: "piece22.jpg",
       id: "22"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece23.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006618/PuzzleHome/Hard/piece23_fmxrpt.jpg`,
       alt: "piece23.jpg",
       id: "23"
     },
     {
       className: "piece-img",
-      src: `./src/pages/Hard/assets/piece24.jpg`,
+      src: `https://res.cloudinary.com/daizywafg/image/upload/v1705006621/PuzzleHome/Hard/piece24_tlcmta.jpg`,
       alt: "piece24.jpg",
       id: "24"
     }
